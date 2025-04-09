806d8cb02550c7fd41ddc4b61a716fda58e3fa2f
charliecatxph <charliecatxph@gmail.com>
2025-04-09 15:13:22 +0800

    create hero page customization

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    router.push(`/attend/${process.env.NEXT_PUBLIC_REDIRECT}`);
+    router.push(`/attend`);
   }, []);
   return (
     <>

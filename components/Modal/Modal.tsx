422b760c71072f62c18fad79cf810715dce32a6e
charliecatxph <charliecatxph@gmail.com>
2025-04-09 15:30:23 +0800

    change modal parameters

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -91,7 +91,7 @@ export const Modal = ({
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="modal-container fixed top-0 left-0 h-full w-full bg-slate-900/80 z-[9999] grid place-content-center inter"
+          className="modal-container fixed top-0 left-0 h-full w-full bg-slate-900/80 z-[9999] flex justify-center items-end md:items-center inter px-5"
         >
           <motion.div
             initial={{ scale: 0.9, opacity: 0 }}
@@ -102,9 +102,9 @@ export const Modal = ({
             }}
             exit={{ scale: 0.9, opacity: 0 }}
             key={2}
-            className="modal bg-white rounded-2xl w-[500px] overflow-hidden"
+            className="modal bg-white rounded-2xl w-full md:w-[600px] overflow-hidden mb-5"
           >
-            <div className="flex items-center gap-5  px-7 py-5 min-h-[170px]">
+            <div className="flex flex-col md:flex-row items-center gap-5  px-7 py-5 min-h-[170px]">
               <div className="">
                 <div className="p-3 rounded-full bg-emerald-50 text-emerald-600">
                   {" "}
@@ -112,22 +112,24 @@ export const Modal = ({
                 </div>
               </div>
               <div className="">
-                <h1 className="font-[500] mt-1">{title}</h1>
-                <p className="text-sm mt-2 text-neutral-800">{content}</p>
+                <h1 className="font-[500] mt-1 text-center md:text-left">
+                  {title}
+                </h1>
+                <p className="text-sm mt-2 text-neutral-800 text-center md:text-left">
+                  {content}
+                </p>
               </div>
             </div>
             <div className="flex justify-end items-center bg-neutral-50 px-4 py-2">
               <div className="flex gap-2 text-sm">
-                {cancelText && (
-                  <button
-                    onClick={() => {
-                      onCancel && onCancel();
-                    }}
-                    className="hover:bg-neutral-50 bg-white border-1 font-[500] border-neutral-400 px-4 py-1.5 rounded-md"
-                  >
-                    {cancelText}
-                  </button>
-                )}
+                <button
+                  onClick={() => {
+                    onCancel && onCancel();
+                  }}
+                  className="hover:bg-neutral-50 bg-white border-1 font-[500] border-neutral-400 px-4 py-1.5 rounded-md"
+                >
+                  {cancelText}
+                </button>
                 <button
                   onClick={() => (onConfirm ? onConfirm() : "")}
                   className="hover:bg-emerald-500 bg-emerald-600 text-white font-[500] px-4 py-1.5 rounded-md"
